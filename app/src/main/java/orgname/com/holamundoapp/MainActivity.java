1b966d79d94272c6a97fd664c5add4ba937f5776
Isaac Yriarte <isaacyriarte@gmail.com>
2017-08-29 16:25:30 -0400

    Se agregan los metodos para mostrar un mensaje generico por pantalla

--- app/src/main/java/orgname/com/holamundoapp/MainActivity.java
+++ app/src/main/java/orgname/com/holamundoapp/MainActivity.java
@@ -1,7 +1,10 @@
 package orgname.com.holamundoapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    
+    public void btnShowAlert_click(View vista){
+
+        showAlert("Mensaje a mostrar","Mensaje");
+    }
+
+    public void showAlert(String titulo, String mensaje){
+        AlertDialog msjAD = new AlertDialog.Builder(this).create();
+        msjAD.setTitle(titulo);
+        msjAD.setMessage(mensaje);
+        msjAD.setButton(DialogInterface.BUTTON_NEUTRAL, "Cerrar", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+
+            }
+        });
+        msjAD.show();
+    }
 }
